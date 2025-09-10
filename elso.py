738c056168f1d7df8a38e4c360943e0b272f355c
Danika20070417 <danielvegh861@gmail.com>
2025-09-10 12:25:22 +0200

    szam lista generálás

---
+++ elso.py
@@ -0,0 +1,14 @@
+import random
+
+
+#lista létrehozása
+szamok = []
+
+#lista feltöltése 10 db random két jegyű egész számokkal
+for i in range(100):
+    szam = random.randint(10, 99)
+    szamok.append(szam)
+
+#Ellenőrzés
+print(szamok)
+
